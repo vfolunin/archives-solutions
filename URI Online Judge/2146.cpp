19bd2e47d4ec2037c6dc52b51c90ade36b823778
vfolunin <v.folunin@gmail.com>
2021-10-15 18:24:51 +0300

    URI solutions added

---
+++ URI Online Judge/2146.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <string>
+using namespace std;
+
+bool solve() {
+    int n;
+    if (!(cin >> n))
+        return 0;
+
+    cout << n - 1 << "\n";
+    return 1;
+}
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+    while (solve());
+}
