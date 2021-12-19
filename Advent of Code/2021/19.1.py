06b3f90c005d9c9f519f1ddb8d6c2ae8bfcf1e25
vfolunin <v.folunin@gmail.com>
2021-12-19 20:21:37 +0300

    Advent solutions added

---
+++ Advent of Code/2021/19.1.py
@@ -0,0 +1,95 @@
+from sys import stdin
+
+class Scanner:
+    def __init__(self, data):
+        lines = data.split('\n')
+        self.id = lines[0].split()[2]
+        self.p = []
+        for line in lines[1:]:
+            self.p.append([int(_) for _ in line.split(',')])
+
+    def rotate_xy(self):
+        for i in range(len(self.p)):
+            self.p[i][0], self.p[i][1] = -self.p[i][1], self.p[i][0]
+
+    def rotate_xz(self):
+        for i in range(len(self.p)):
+            self.p[i][0], self.p[i][2] = -self.p[i][2], self.p[i][0]
+
+    def rotate_yz(self):
+        for i in range(len(self.p)):
+            self.p[i][1], self.p[i][2] = -self.p[i][2], self.p[i][1]
+
+    def anchor_intersection(self, that, a, b):
+        dx = self.p[a][0] - that.p[b][0]
+        dy = self.p[a][1] - that.p[b][1]
+        dz = self.p[a][2] - that.p[b][2]
+        s = set(tuple(p) for p in self.p)
+        res = 0
+        for i in range(len(that.p)):
+            if res + len(that.p) - i < 12:
+                return 0
+            x, y, z = that.p[i]
+            res += (x + dx, y + dy, z + dz) in s
+        return res
+
+    def intersection(self, that):
+        res, ra, rb = 0, 0, 0
+        for a in range(len(self.p)):
+            for b in range(len(that.p)):
+                cur = self.anchor_intersection(that, a, b)
+                if res < cur:
+                    res, ra, rb = cur, a, b
+        return res, ra, rb
+
+    def max_intersection(self, that):
+        res, rx, ry, rz, ra, rb = 0, 0, 0, 0, 0, 0
+        for x in range(4):
+            for y in range(4):
+                for z in range(4):
+                    cur, a, b = self.intersection(that)
+                    if res < cur:
+                        res, rx, ry, rz, ra, rb = cur, x, y, z, a, b
+                    that.rotate_xy()
+                that.rotate_xz()
+            that.rotate_yz()
+        return res, rx, ry, rz, ra, rb
+
+    def merge(self, that):
+        res, rx, ry, rz, ra, rb = self.max_intersection(that)
+        if res < 12:
+            return False
+
+        for x in range(rx):
+            that.rotate_yz()
+        for y in range(ry):
+            that.rotate_xz()
+        for z in range(rz):
+            that.rotate_xy()
+
+        dx = self.p[ra][0] - that.p[rb][0]
+        dy = self.p[ra][1] - that.p[rb][1]
+        dz = self.p[ra][2] - that.p[rb][2]
+        s = set(tuple(p) for p in self.p)
+        for x, y, z in that.p:
+            t = [x + dx, y + dy, z + dz]
+            if tuple(t) not in s:
+                self.p.append(t)
+
+        return True
+
+
+scanner_data = ''.join(stdin.readlines()).split('\n\n')
+scanners = [Scanner(data) for data in scanner_data]
+
+while len(scanners) > 1:
+    for i in range(len(scanners)):
+        for j in range(i + 1, len(scanners)):
+            if scanners[i].merge(scanners[j]):
+                print('Merging {} and {} (remaining: {})'.format(scanners[i].id, scanners[j].id, len(scanners)))
+                scanners.pop(j)
+                break
+            else:
+                print('Can\'t merge {} and {} (remaining: {})'.format(scanners[i].id, scanners[j].id, len(scanners)))
+
+print(len(scanners[0].p))
