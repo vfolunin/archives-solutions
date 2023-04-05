610e851f7e9d58cabd0827aca8879e85876ff2c3
vfolunin <v.folunin@gmail.com>
2023-04-05 14:47:39 +0300

    MCCME solutions added

---
+++ MCCME/113163.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <string>
+using namespace std;
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+    string s;
+    cin >> s;
+
+    int sum = 0;
+    for (char c : s)
+        sum += c;
+
+    cout << sum;
+}
