1923169b42a39d0a7a7ec97f3949e793e9ab5a9a
vfolunin <v.folunin@gmail.com>
2023-07-08 21:46:19 +0300

    ACMP solutions added

---
+++ ACMP/195.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <string>
+using namespace std;
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+    int n, h, w;
+    cin >> n >> h >> w;
+
+    cout << 2 * n * h * w;
+}
