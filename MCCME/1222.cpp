9b4708b849233df0bad0bfba80672f8a8886d75d
vfolunin <v.folunin@gmail.com>
2023-10-13 19:27:46 +0300

    MCCME solutions added

--- MCCME/1222.cpp
+++ MCCME/1222.cpp
@@ -8,12 +8,14 @@ using namespace std;
 
 bool gcd(long long a, long long b, long long c, long long d) {
     while (1) {
+        if (a == c && b == d)
+            return 1;
         if (a < b)
             swap(a, b);
         if (!b)
             return 0;
         if (b == d)
-            return a > c && (a - c) % d == 0;
+            return a >= c && (a - c) % b == 0;
         a %= b;
     }
     return 0;
