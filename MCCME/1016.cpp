9cbb7f02c612a3d69d282443875c7997adc3395d
vfolunin <v.folunin@gmail.com>
2023-10-29 11:44:34 +0300

    MCCME solutions added

---
+++ MCCME/1016.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <string>
+using namespace std;
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+ 
+    int size;
+    cin >> size;
+
+    multiset<pair<int, int>> counts;
+    for (int i = 0; i < size; i++) {
+        int count;
+        cin >> count;
+        counts.insert({ count, i + 1 });
+    }
+
+    vector<pair<int, int>> res;
+    while (counts.size() > 1) {
+        auto [countA, idA] = *prev(counts.end());
+        counts.erase(prev(counts.end()));
+        auto [countB, idB] = *prev(counts.end());
+        counts.erase(prev(counts.end()));
+
+        if (countA > 1)
+            counts.insert({ countA - 1, idA });
+        if (countB > 1)
+            counts.insert({ countB - 1, idB });
+
+        res.push_back({ idA, idB });
+    }
+
+    cout << res.size() << "\n";
+    for (auto &[idA, idB] : res)
+        cout << idA << " " << idB << "\n";
+}
