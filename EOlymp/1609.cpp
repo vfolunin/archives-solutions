a10b1df02463dc7c5eb7efcfb0088b571014eacd
vfolunin <v.folunin@gmail.com>
2024-01-03 08:47:44 +0400

    EOlymp solutions added

---
+++ EOlymp/1609.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <string>
+using namespace std;
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+    string s;
+    char c;
+    cin >> s >> c;
+
+    cout << count(s.begin(), s.end(), c);
+}
