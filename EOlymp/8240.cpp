a11fae6340cbfeebbd0ecce8d9db06c2ba1f1791
vfolunin <v.folunin@gmail.com>
2024-01-23 09:54:33 +0300

    EOlymp solutions added

---
+++ EOlymp/8240.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <set>
+#include <map>
+#include <string>
+using namespace std;
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+    for (double x; cin >> x; )
+        cout << fixed << sqrt(x) + 2 * x + sin(x) << "\n";
+}
---
+++ EOlymp/8912.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <string>
+using namespace std;
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+    int res = 0;
+    for (int value; cin >> value && value; )
+        if (value > 0)
+            res += value;
+
+    cout << res;
+}
