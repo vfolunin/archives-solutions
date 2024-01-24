c15fc964698001e766c063b882fe660ae513e76a
vfolunin <v.folunin@gmail.com>
2024-01-24 10:20:52 +0300

    EOlymp solutions added

---
+++ EOlymp/3981.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <string>
+using namespace std;
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+    int vertexCount;
+    cin >> vertexCount;
+
+    vector<vector<int>> graph(vertexCount);
+    for (int a = 0; a < vertexCount; a++) {
+        for (int b = 0; b < vertexCount; b++) {
+            int hasEdge;
+            cin >> hasEdge;
+            if (hasEdge)
+                graph[a].push_back(b);
+        }
+    }
+
+    for (int v = 0; v < vertexCount; v++) {
+        cout << graph[v].size() << " ";
+        for (int to : graph[v])
+            cout << to + 1 << " ";
+        cout << "\n";
+    }
+}
