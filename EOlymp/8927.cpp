bacd16f626ef80d6964c707a40ab53ddee966d86
vfolunin <v.folunin@gmail.com>
2024-02-03 10:55:18 +0300

    EOlymp solutions added

---
+++ EOlymp/8927.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <string>
+using namespace std;
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+ 
+    int n;
+    cin >> n;
+
+    for (int d = 2; d * d <= n; d++) {
+        if (n % d == 0) {
+            cout << d;
+            return 0;
+        }
+    }
+
+    cout << n;
+}
