654a18d5cbb1402e5effdb0bd1c6b48372e8b3f2
vfolunin <v.folunin@gmail.com>
2024-02-05 15:50:33 +0300

    EOlymp solutions added

---
+++ EOlymp/7817.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <string>
+using namespace std;
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+ 
+    int n;
+    cin >> n;
+
+    long long res = 1;
+    for (int i = 0; i < n; i++)
+        res *= 5;
+
+    cout << res;
+}
---
+++ EOlymp/8280.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <string>
+using namespace std;
+
+vector<int> getDivisors(int n) {
+    vector<int> divisors;
+
+    for (long long d = 1; d * d <= n; d++) {
+        if (n % d == 0) {
+            divisors.push_back(d);
+            if (d * d != n)
+                divisors.push_back(n / d);
+        }
+    }
+
+    sort(divisors.begin(), divisors.end());
+    return divisors;
+}
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+    int n;
+    cin >> n;
+
+    int res = 0;
+    for (int d : getDivisors(n))
+        res += d % 2 == 0;
+
+    cout << res;
+}
