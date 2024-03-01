8872ffb9b7e9a72f6e9b7688a1c900110801dcb8
vfolunin <v.folunin@gmail.com>
2024-03-01 17:19:37 +0300

    EOlymp solutions added

---
+++ EOlymp/4429.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <string>
+using namespace std;
+
+struct Event {
+    int time;
+    enum { END, BEGIN } type;
+
+    bool operator < (const Event &that) const {
+        if (time != that.time)
+            return time < that.time;
+        return type < that.type;
+    }
+};
+
+int readTime() {
+    int h, m, s;
+    cin >> h >> m >> s;
+    return h * 60 * 60 + m * 60 + s;
+}
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+    int ticketOfficeCount;
+    cin >> ticketOfficeCount;
+
+    vector<Event> events;
+
+    for (int i = 0; i < ticketOfficeCount; i++) {
+        int t1 = readTime(), t2 = readTime();
+        if (t1 == t2) {
+            events.push_back({ 0, Event::BEGIN });
+            events.push_back({ 24 * 60 * 60, Event::END });
+        } else if (t1 < t2) {
+            events.push_back({ t1, Event::BEGIN });
+            events.push_back({ t2, Event::END });
+        } else {
+            events.push_back({ t1, Event::BEGIN });
+            events.push_back({ 24 * 60 * 60, Event::END });
+            events.push_back({ 0, Event::BEGIN });
+            events.push_back({ t2, Event::END });
+        }
+    }
+
+    sort(events.begin(), events.end());
+
+    int prevTime = 0, segmentCount = 0, res = 0;
+    for (auto &[time, type] : events) {
+        if (segmentCount == ticketOfficeCount)
+            res += time - prevTime;
+
+        if (type == Event::BEGIN)
+            segmentCount++;
+        else
+            segmentCount--;
+
+        prevTime = time;
+    }
+
+    cout << res;
+}
---
+++ EOlymp/835.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <unordered_map>
+#include <string>
+using namespace std;
+
+int main() {
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+
+	vector<int> a(5);
+	unordered_map<int, int> count;
+	for (int &value : a) {
+		cin >> value;
+		count[value]++;
+	}
+
+	sort(a.begin(), a.end());
+
+	vector<int> counts(6);
+	for (auto &[value, count] : count)
+		counts[count]++;
+
+	if (counts[5])
+		cout << "Impossible";
+	else if (counts[4])
+		cout << "Four of a Kind";
+	else if (counts[2] && counts[3])
+		cout << "Full House";
+	else if (a[0] + 1 == a[1] && a[0] + 2 == a[2] && a[0] + 3 == a[3] && a[0] + 4 == a[4])
+		cout << "Straight";
+	else if (counts[3])
+		cout << "Three of a Kind";
+	else if (counts[2] == 2)
+		cout << "Two Pairs";
+	else if (counts[2])
+		cout << "One Pair";
+	else
+		cout << "Nothing";
+}
