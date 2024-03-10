c69ba03636971a91a71918a141ba6c73dcdad467
vfolunin <v.folunin@gmail.com>
2024-03-10 05:55:23 +0300

    EOlymp solutions added

---
+++ EOlymp/4103.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <string>
+using namespace std;
+
+int fromRoman(string &s) {
+    static const vector<pair<string, int>> CODES = {
+        {"M", 1000}, {"CM", 900}, {"D", 500}, {"CD", 400},
+        {"C", 100}, {"XC", 90}, {"L", 50}, {"XL", 40},
+        {"X", 10}, {"IX", 9}, {"V", 5}, {"IV", 4}, {"I", 1}
+    };
+
+    int pos = 0, res = 0;
+    for (auto &[code, value] : CODES) {
+        while (s.substr(pos, code.size()) == code) {
+            pos += code.size();
+            res += value;
+        }
+    }
+    return res;
+}
+
+void solve() {
+    string s;
+    cin >> s;
+
+    cout << fromRoman(s) << "\n";
+}
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+        solve();
+}
