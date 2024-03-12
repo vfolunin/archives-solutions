1a2669f81ac5a31540803a7924243433f84e21f4
vfolunin <v.folunin@gmail.com>
2024-03-12 09:15:40 +0300

    EOlymp solutions added

---
+++ EOlymp/4176.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <string>
+using namespace std;
+
+int main() {
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+
+	string a, b;
+	cin >> a >> b;
+
+	vector<pair<int, int>> res;
+	for (int l = 0; l < a.size(); l++) {
+		if (a[l] == b[l])
+			continue;
+
+		int r = a.find(b[l], l);
+		reverse(a.begin() + l, a.begin() + r + 1);
+		res.push_back({ l, r });
+	}
+
+	cout << res.size() << "\n";
+	for (auto &[l, r] : res)
+		cout << l + 1 << " " << r + 1 << "\n";
+}
