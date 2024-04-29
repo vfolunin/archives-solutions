678e28eed163525109315d37b7c7557825d47cac
vfolunin <v.folunin@gmail.com>
2024-04-29 22:01:40 +0400

    EOlymp solutions added

---
+++ EOlymp/1744.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <string>
+using namespace std;
+
+void solve() {
+    long long vertexCount, edgeCount;
+    cin >> vertexCount >> edgeCount;
+
+    cout << (vertexCount >= 1 && edgeCount <= (vertexCount - 1) * (vertexCount - 2) / 2 ? "Yes\n" : "No\n");
+}
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+        solve();
+}
---
+++ EOlymp/3742.py
@@ -0,0 +1,3 @@
+n, p = [int(_) for _ in input().split()]
+
+print(pow(n, n, 10 ** p))
