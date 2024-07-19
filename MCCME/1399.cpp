a7c59278e8ae2eb7136814d0c703b5e9f2d3853c
vfolunin <v.folunin@gmail.com>
2024-07-19 20:38:13 +0300

    MCCME solutions added

---
+++ MCCME/1399.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <string>
+using namespace std;
+
+int ones(int n) {
+    int res = 0;
+    while (n) {
+        res++;
+        n &= n - 1;
+    }
+    return res;
+}
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+    int n;
+    cin >> n;
+
+    cout << (1LL << (ones(n) - 1));
+}
