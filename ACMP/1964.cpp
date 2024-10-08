9f966eeb65ef8591c67ce6127ed029ce2dc89688
vfolunin <v.folunin@gmail.com>
2024-10-08 21:45:44 +0300

    ACMP solutions added

---
+++ ACMP/1964.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <string>
+using namespace std;
+
+long long toDec(string s, int base) {
+    static const string DIGITS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    long long res = 0;
+    for (char c : s)
+        res = res * base + DIGITS.find(c);
+    return res;
+}
+
+string fromDec(long long n, int base) {
+    static const string DIGITS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    string res;
+    while (n >= base) {
+        res += DIGITS[n % base];
+        n /= base;
+    }
+    res += DIGITS[n];
+    reverse(res.begin(), res.end());
+    return res;
+}
+
+long long transform(long long value) {
+    string s = fromDec(value, 2);
+    s += s.back();
+    s += count(s.begin(), s.end(), '1') % 2 ? "11" : "01";
+    return toDec(s, 2);
+}
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+    int limit;
+    cin >> limit;
+
+    int res = 1;
+    while (transform(res) <= limit)
+        res++;
+
+    cout << res;
+}
---
+++ ACMP/1966.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <string>
+using namespace std;
+
+long long toDec(string s, int base) {
+    static const string DIGITS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    long long res = 0;
+    for (char c : s)
+        res = res * base + DIGITS.find(c);
+    return res;
+}
+
+string fromDec(long long n, int base) {
+    static const string DIGITS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    string res;
+    while (n >= base) {
+        res += DIGITS[n % base];
+        n /= base;
+    }
+    res += DIGITS[n];
+    reverse(res.begin(), res.end());
+    return res;
+}
+
+long long transform(long long value) {
+    string s = fromDec(value, 2);
+    if (value % 2)
+        s = "11" + s + "0";
+    else
+        s = "1" + s + "10";
+    return toDec(s, 2);
+}
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+    int limit;
+    cin >> limit;
+
+    int res = 1e9;
+    for (int i = 1; i < 2e5; i++) {
+        int value = transform(i);
+        if (value > limit)
+            res = min(res, value);
+    }
+
+    cout << res;
+}
