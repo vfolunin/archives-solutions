014f1e568784fe5bef5419afb5411d8780e54b91
vfolunin <v.folunin@gmail.com>
2024-10-23 20:26:27 +0300

    LeetCode solutions added

--- LeetCode/2641.cpp
+++ LeetCode/2641.cpp
@@ -1,33 +1,34 @@
 class Solution {
-    void rec1(TreeNode *n, TreeNode *p, int depth, unordered_map<int, int> &depthSum, unordered_map<TreeNode *, int> &parentSum) {
+    void rec1(TreeNode *n, TreeNode *p, int depth, vector<int> &depthSum) {
         if (!n)
             return;
 
+        if (depthSum.size() == depth)
+            depthSum.emplace_back();
         depthSum[depth] += n->val;
-        parentSum[p] += n->val;
 
-        rec1(n->left, n, depth + 1, depthSum, parentSum);
-        rec1(n->right, n, depth + 1, depthSum, parentSum);
+        rec1(n->left, n, depth + 1, depthSum);
+        rec1(n->right, n, depth + 1, depthSum);
     }
 
-    void rec2(TreeNode *n, TreeNode *p, int depth, unordered_map<int, int> &depthSum, unordered_map<TreeNode *, int> &parentSum) {
+    void rec2(TreeNode *n, TreeNode *p, int depth, vector<int> &depthSum, int parentSum) {
         if (!n)
             return;
 
-        n->val = depthSum[depth] - parentSum[p];
+        n->val = depthSum[depth] - parentSum;
+        int childSum = (n->left ? n->left->val : 0) + (n->right ? n->right->val : 0);
 
-        rec2(n->left, n, depth + 1, depthSum, parentSum);
-        rec2(n->right, n, depth + 1, depthSum, parentSum);
+        rec2(n->left, n, depth + 1, depthSum, childSum);
+        rec2(n->right, n, depth + 1, depthSum, childSum);
     }
 
 public:
     TreeNode *replaceValueInTree(TreeNode *root) {
-        unordered_map<int, int> depthSum;
-        unordered_map<TreeNode *, int> parentSum;
+        vector<int> depthSum;
 
-        rec1(root, 0, 0, depthSum, parentSum);
-        rec2(root, 0, 0, depthSum, parentSum);
-
+        rec1(root, 0, 0, depthSum);
+        rec2(root, 0, 0, depthSum, root->val);
+        
         return root;
     }
 };
---
+++ LeetCode/3329.cpp
@@ -0,0 +1,28 @@
+struct Counter {
+    unordered_map<char, int> count;
+    multiset<int> counts;
+
+    void add(char c, int delta) {
+        if (auto it = counts.find(count[c]); it != counts.end())
+            counts.erase(it);
+        count[c] += delta;
+        counts.insert(count[c]);
+    }
+};
+
+class Solution {
+public:
+    long long numberOfSubstrings(string &s, int threshold) {
+        Counter counter;
+        long long res = 0;
+        
+        for (int l = 0, r = 0; r < s.size(); r++) {
+            counter.add(s[r], 1);
+            while (*counter.counts.rbegin() >= threshold)
+                counter.add(s[l++], -1);
+            res += l;
+        }
+
+        return res;
+    }
+};
