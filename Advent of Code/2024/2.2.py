68b381bfc0f39f03951692a5d25f4fe1034b7caf
vfolunin <v.folunin@gmail.com>
2024-12-02 20:31:14 +0300

    Advent solutions added

---
+++ Advent of Code/2024/2.2.py
@@ -0,0 +1,42 @@
+from sys import stdin
+
+
+def get_type(a, b):
+    if a < b:
+        return -1
+    elif a == b:
+        return 0
+    else:
+        return 1
+
+
+def check(a):
+    if get_type(a[0], a[1]) == 0:
+        return 0
+
+    for i in range(1, len(a)):
+        delta = abs(a[i - 1] - a[i])
+        if delta < 1 or delta > 3 or get_type(a[0], a[1]) != get_type(a[i - 1], a[i]):
+            return 0
+
+    return 1
+
+
+def is_safe(a):
+    if check(a):
+        return 1
+
+    for i in range(len(a)):
+        if check(a[:i] + a[i + 1:]):
+            return 1
+
+    return 0
+
+
+res = 0
+
+for line in stdin:
+    a = [int(_) for _ in line.split()]
+    res += is_safe(a)
+
+print(res)
