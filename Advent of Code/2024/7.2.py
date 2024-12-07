67130f344b5a6918d2f8d39ac1a1142b130d6a1a
vfolunin <v.folunin@gmail.com>
2024-12-07 23:44:25 +0300

    Advent solutions added

---
+++ Advent of Code/2024/7.2.py
@@ -0,0 +1,21 @@
+from sys import stdin
+
+
+def rec(target, cur, a, i):
+    if i == len(a):
+        return target == cur
+    return (rec(target, cur + a[i], a, i + 1) or
+            rec(target, cur * a[i], a, i + 1) or
+            rec(target, int(str(cur) + str(a[i])), a, i + 1))
+
+
+res = 0
+
+for line in stdin:
+    target, a = line.strip().split(': ')
+    target = int(target)
+    a = [int(_) for _ in a.split()]
+    if rec(target, a[0], a,1):
+        res += target
+
+print(res)
