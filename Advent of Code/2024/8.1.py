bfd49b93a1fe195b98fd308a1742bf3ac5175904
vfolunin <v.folunin@gmail.com>
2024-12-08 21:03:28 +0300

    Advent solutions added

---
+++ Advent of Code/2024/8.1.py
@@ -0,0 +1,23 @@
+from collections import defaultdict
+from sys import stdin
+
+a = [line.strip() for line in stdin]
+
+cells = defaultdict(set)
+for y in range(len(a)):
+    for x in range(len(a[0])):
+        if a[y][x] != '.':
+            cells[a[y][x]].add((y, x))
+
+res = set()
+for cells in cells.values():
+    for y1, x1 in cells:
+        for y2, x2 in cells:
+            if y1 == y2 and x1 == x2:
+                continue
+            y = y2 + y2 - y1
+            x = x2 + x2 - x1
+            if 0 <= y < len(a) and 0 <= x < len(a[0]):
+                res.add((y, x))
+
+print(len(res))
