a2f47c44416cf76a5714bd73cc839a5c30b0b721
vfolunin <v.folunin@gmail.com>
2025-01-13 19:34:17 +0400

    SPOJ solutions added

---
+++ SPOJ/elis.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <string>
+using namespace std;
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+    int size;
+    cin >> size;
+
+    vector<int> a(size);
+    for (int &value : a)
+        cin >> value;
+
+    vector<int> res(size, 1);
+    for (int i = 1; i < a.size(); i++)
+        for (int j = 0; j < i; j++)
+            if (a[j] < a[i])
+                res[i] = max(res[i], res[j] + 1);
+
+    cout << *max_element(res.begin(), res.end());
+}
