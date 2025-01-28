1ddd07e8d6dc81b71c2528bc0afced6a6bba36dd
vfolunin <v.folunin@gmail.com>
2025-01-28 05:46:17 +0300

    SPOJ solutions added

---
+++ SPOJ/fastpow.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <string>
+using namespace std;
+
+long long binPow(long long x, long long p, long long mod) {
+    if (!p)
+        return 1 % mod;
+    if (p % 2)
+        return binPow(x, p - 1, mod) * x % mod;
+    long long r = binPow(x, p / 2, mod);
+    return r * r % mod;
+}
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+    long long n, p, mod;
+    cin >> n >> p >> mod;
+
+    cout << binPow(n, p, mod);
+}
