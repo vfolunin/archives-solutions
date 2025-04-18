a79ed28de2f21e804fed7d93c830cbd839ae23f6
vfolunin <v.folunin@gmail.com>
2025-04-18 18:55:47 +0300

    EOlymp solutions added

---
+++ EOlymp/1869.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <string>
+using namespace std;
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+ 
+    int size;
+    cin >> size;
+
+    vector<long long> ways = {
+        1, 4, 12, 36, 100,
+        284, 780, 2172, 5916, 16268,
+        44100, 120292, 324932, 881500, 2374444,
+        6416596, 17245332, 46466676, 124658732, 335116620,
+        897697164, 2408806028, 6444560484
+    };
+
+    cout << ways[size];
+}
