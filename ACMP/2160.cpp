51c12dcb3c3396fb891c7eff436de39f6f54ce61
vfolunin <v.folunin@gmail.com>
2025-08-11 23:01:41 +0400

    ACMP solutions added

---
+++ ACMP/2160.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <string>
+using namespace std;
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+    int n;
+    cin >> n;
+
+    int res = 0;
+    while ((res * res * res + 5 * res + 6) / 6 < n)
+        res++;
+
+    cout << res;
+}
