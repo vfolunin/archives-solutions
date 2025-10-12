1947d21ba73c6b3c6873c66bd428ebc49789673a
vfolunin <v.folunin@gmail.com>
2025-10-12 16:17:50 +0300

    SPOJ solutions added

---
+++ SPOJ/pucmm001.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <string>
+using namespace std;
+
+map<int, int> factorize(int n) {
+    map<int, int> factorization;
+
+    for (long long d = 2; d * d <= n; d++) {
+        while (n % d == 0) {
+            factorization[d]++;
+            n /= d;
+        }
+    }
+    if (n != 1)
+        factorization[n]++;
+
+    return factorization;
+}
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+    int size;
+    cin >> size;
+
+    for (int i = 0; i < size; i++) {
+        int value;
+        cin >> value;
+
+        if (factorize(value).size() < 3) {
+            cout << "NO";
+            return 0;
+        }
+    }
+
+    cout << "YES";
+}
