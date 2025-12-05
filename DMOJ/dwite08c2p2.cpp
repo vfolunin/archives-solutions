b093e76b278e4530b42eef4e3be44d2d3369b9d3
vfolunin <v.folunin@gmail.com>
2025-12-05 19:51:56 +0300

    DMOJ solutions added

---
+++ DMOJ/dwite08c2p2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <string>
+using namespace std;
+
+bool solve() {
+    string s;
+    if (!(cin >> s))
+        return 0;
+
+    cout << *max_element(s.begin(), s.end()) << "\n";
+    return 1;
+}
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+    while (solve());
+}
---
+++ DMOJ/othscc1p2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <string>
+using namespace std;
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+    int size;
+    cin >> size;
+
+    int sum = 0, maxValue = 0;
+    for (int i = 0; i < size; i++) {
+        int value;
+        cin >> value;
+
+        sum += value;
+        maxValue = max(maxValue, value);
+    }
+
+    cout << (sum - maxValue) / (size - 1);
+}
