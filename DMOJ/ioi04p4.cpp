a6f89612974bc7817dd22caa6e0debd8d94e2851
vfolunin <v.folunin@gmail.com>
2026-04-12 19:53:24 +0300

    DMOJ solutions added

---
+++ DMOJ/ioi04p4.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <string>
+using namespace std;
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+    int height, width, partCount;
+    cin >> height >> width >> partCount;
+
+    set<pair<int, int>> parts;
+    for (int i = 0; i < partCount; i++) {
+        int height, width;
+        cin >> height >> width;
+
+        parts.insert({ height, width });
+    }
+
+    vector<vector<int>> res(height + 1, vector<int>(width + 1));
+    for (int h = 1; h <= height; h++) {
+        for (int w = 1; w <= width; w++) {
+            if (parts.count({ h, w })) {
+                res[h][w] = 0;
+                continue;
+            }
+
+            res[h][w] = h * w;
+            for (int h1 = 1, h2 = h - 1; h1 <= h2; h1++, h2--)
+                res[h][w] = min(res[h][w], res[h1][w] + res[h2][w]);
+            for (int w1 = 1, w2 = w - 1; w1 <= w2; w1++, w2--)
+                res[h][w] = min(res[h][w], res[h][w1] + res[h][w2]);
+        }
+    }
+
+    cout << res[height][width];
+}
