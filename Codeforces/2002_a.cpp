cac62405fdae42d4591d38d4d8cd6beee7619169
vfolunin <v.folunin@gmail.com>
2026-05-23 22:26:11 +0300

    Codeforces solutions added

---
+++ Codeforces/2002_a.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <string>
+using namespace std;
+
+void solve() {
+    int h, w, k;
+    cin >> h >> w >> k;
+
+    cout << min(h, k) * min(w, k) << "\n";
+}
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+    int testCount;
+    cin >> testCount;
+
+    for (int test = 0; test < testCount; test++)
+        solve();
+}
---
+++ Codeforces/2034_a.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <string>
+#include <numeric>
+using namespace std;
+
+void solve() {
+    int a, b;
+    cin >> a >> b;
+
+    cout << lcm(a, b) << "\n";
+}
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+    int testCount;
+    cin >> testCount;
+
+    for (int test = 0; test < testCount; test++)
+        solve();
+}
