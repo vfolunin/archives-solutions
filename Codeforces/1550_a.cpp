0df4cb612089a976c3cc99f69c3cda849be2c55d
vfolunin <v.folunin@gmail.com>
2026-05-28 07:55:46 +0300

    Codeforces solutions added

---
+++ Codeforces/1550_a.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <set>
+#include <map>
+#include <string>
+using namespace std;
+
+void solve() {
+    int n;
+    cin >> n;
+
+    cout.precision(0);
+    cout << fixed << ceil(sqrt(n)) << "\n";
+}
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+    int testCount;
+    cin >> testCount;
+
+    for (int test = 0; test < testCount; test++)
+        solve();
+}
---
+++ Codeforces/1566_a.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <string>
+using namespace std;
+
+void solve() {
+    int size, sum;
+    cin >> size >> sum;
+
+    size -= (size - 1) / 2;
+    int res = sum / size;
+
+    cout << res << "\n";
+}
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+    int testCount;
+    cin >> testCount;
+
+    for (int test = 0; test < testCount; test++)
+        solve();
+}
