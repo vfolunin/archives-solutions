d08a4adc7f27299889939bff1ee83ce3dad02864
vfolunin <v.folunin@gmail.com>
2026-05-31 18:59:09 +0300

    MCCME solutions added

---
+++ MCCME/115237.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <string>
+using namespace std;
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+    string s;
+    cin >> s;
+
+    vector<int> letterCount(26);
+    for (char c : s)
+        letterCount[c - 'a']++;
+
+    int ruleCount;
+    cin >> ruleCount;
+
+    vector<int> pressCount(26);
+    for (int i = 0; i < ruleCount; i++) {
+        char c;
+        cin >> c >> pressCount[c - 'a'];
+        pressCount[c - 'a']--;
+    }
+
+    int res = 0;
+    for (int i = 0; i < letterCount.size(); i++) {
+        res += letterCount[i];
+        if (pressCount[i])
+            res += (letterCount[i] + pressCount[i] - 1) / pressCount[i];
+    }
+    
+    cout << res;
+}
