3783a86fd2c70db51fa1379fe49dbef1ca532ef9
vfolunin <v.folunin@gmail.com>
2026-07-03 17:55:36 +0300

    Codeforces solutions added

---
+++ Codeforces/875_b.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <unordered_set>
+#include <map>
+#include <string>
+using namespace std;
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+    int size;
+    cin >> size;
+
+    cout << "1 ";
+
+    unordered_set<int> onePos;
+    int lastPos = size;
+
+    for (int i = 0; i < size; i++) {
+        int pos;
+        cin >> pos;
+
+        onePos.insert(pos);
+        while (onePos.count(lastPos))
+            onePos.erase(lastPos--);
+
+        cout << onePos.size() + 1 << " ";
+    }
+}
