8a640818330203382ed5c06433e504efbac92263
vfolunin <v.folunin@gmail.com>
2026-07-22 18:12:39 +0300

    Kattis solutions added

--- Kattis/parket.cpp
+++ Kattis/parket.cpp
@@ -6,34 +6,22 @@
 #include <string>
 using namespace std;
 
-int daysInMonth(int m) {
-    vector<int> days = { 0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-    return days[m];
-}
-
-int rec(int d, int m) {
-    if (m > 12 || d > daysInMonth(m))
-        return 1;
-
-    static vector<vector<int>> memo(32, vector<int>(13, -1));
-    int &res = memo[d][m];
-    if (res != -1)
-        return res;
-
-    if (d == 31 && m == 12 ||
-        rec(d + 1, m) == 2 || rec(d + 2, m) == 2 ||
-        rec(d, m + 1) == 2 || rec(d, m + 2) == 2)
-        return res = 1;
-
-    return res = 2;
-}
-
 int main() {
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 
-    int d, m;
-    cin >> d >> m;
+    int a, b;
+    cin >> a >> b;
 
-    cout << rec(d, m);
+    for (int h = 2; 1; h++) {
+        if ((a + b) % h)
+            continue;
+
+        int w = (a + b) / h;
+
+        if (a == (h + w - 2) * 2 && b == (h - 2) * (w - 2)) {
+            cout << w << " " << h;
+            return 0;
+        }
+    }
 }
