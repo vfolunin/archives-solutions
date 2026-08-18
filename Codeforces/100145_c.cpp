b8cde713dd2a141ff4130d7bd932cbf69ac911ac
vfolunin <v.folunin@gmail.com>
2026-08-18 04:08:16 +0400

    Codeforces solutions added

---
+++ Codeforces/100145_c.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <string>
+using namespace std;
+
+long long gcdex(long long a, long long b, long long &x, long long &y) {
+    if (!b) {
+        x = 1;
+        y = 0;
+        return a;
+    }
+    long long x1, y1, d = gcdex(b, a % b, x1, y1);
+    x = y1;
+    y = x1 - a / b * y1;
+    return d;
+}
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+    for (long long a, b; cin >> a >> b; ) {
+        long long x, y, d = gcdex(a, b, x, y);
+        cout << x << " " << y << " " << d << "\n";
+    }
+}
---
+++ Codeforces/100184_a.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <string>
+using namespace std;
+
+int grundy(int value) {
+    value %= 5;
+    if (value <= 1)
+        return 0;
+    else if (value <= 3)
+        return 1;
+    else
+        return 2;
+}
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int size;
+    cin >> size;
+
+    int res = 0;
+    for (int i = 0; i < size; i++) {
+        int value;
+        cin >> value;
+
+        res ^= grundy(value);
+    }
+
+    cout << (res ? "Rybka" : "Watson");
+}
