b789d77b9db283a8d7624254c57c1e9898958d10
vfolunin <v.folunin@gmail.com>
2026-08-21 20:20:17 +0300

    LeetCode solutions added

--- LeetCode/3116.cpp
+++ LeetCode/3116.cpp
@@ -2,7 +2,8 @@ class Solution {
     bool can(vector<int> &a, int k, long long sum) {
         long long count = 0;
         for (int mask = 1; mask < (1 << a.size()); mask++) {
-            int ones = 0, divisor = 1;
+            int ones = 0;
+            long long divisor = 1;
             for (int bit = 0; bit < a.size(); bit++) {
                 if (mask & (1 << bit)) {
                     ones++;
