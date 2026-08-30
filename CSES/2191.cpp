16ef7c43709840ad9f16a2ce29ed2ce6c9e84d61
vfolunin <v.folunin@gmail.com>
2026-08-30 21:51:57 +0300

    CSES solutions added

---
+++ CSES/2191.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <string>
+using namespace std;
+
+struct Point {
+    long long x, y;
+
+    long long crossProduct(const Point &that) const {
+        return x * that.y - y * that.x;
+    }
+
+    friend istream &operator >> (istream &in, Point &p) {
+        return in >> p.x >> p.y;
+    }
+};
+
+struct Polygon {
+    vector<Point> points;
+
+    long long area() const {
+        long long s = 0;
+        for (int i = 0; i < points.size(); i++)
+            s += points[i].crossProduct(points[(i + 1) % points.size()]);
+        return abs(s);
+    }
+
+    friend istream &operator >> (istream &in, Polygon &polygon) {
+        int pointCount;
+        in >> pointCount;
+
+        polygon.points.resize(pointCount);
+        for (Point &point : polygon.points)
+            in >> point;
+
+        return in;
+    }
+};
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+    Polygon polygon;
+    cin >> polygon;
+
+    cout << polygon.area();
+}
