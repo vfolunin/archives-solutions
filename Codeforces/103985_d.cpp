8c21d164658504e08213156eecf2fb72a7e4e473
vfolunin <v.folunin@gmail.com>
2026-09-21 20:05:02 +0300

    Codeforces solutions added

---
+++ Codeforces/103985_d.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <unordered_set>
+#include <map>
+#include <string>
+using namespace std;
+
+void dfs(vector<unordered_set<int>> &graph, int v, unordered_set<int> &visited) {
+    visited.insert(v);
+    for (int to : graph[v])
+        if (!visited.contains(to))
+            dfs(graph, to, visited);
+}
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+  
+    int wordCount, letterCount;
+    cin >> wordCount >> letterCount;
+
+    vector<vector<int>> words(wordCount);
+    for (vector<int> &word : words) {
+        int size;
+        cin >> size;
+
+        word.resize(size);
+        for (int &letter : word) {
+            cin >> letter;
+            letter--;
+        }
+    }
+
+    unordered_set<int> starts;
+    vector<unordered_set<int>> graph(letterCount);
+    for (int i = 0; i + 1 < words.size(); i++) {
+        int j = 0;
+        while (j < words[i].size() && j < words[i + 1].size() && words[i][j] == words[i + 1][j])
+            j++;
+
+        if (j == words[i + 1].size() && words[i].size() > words[i + 1].size()) {
+            cout << "No";
+            return 0;
+        }
+
+        if (j < words[i].size()) {
+            if (words[i][j] < words[i + 1][j])
+                graph[words[i + 1][j]].insert(words[i][j]);
+            else
+                starts.insert(words[i][j]);
+        }
+    }
+
+    unordered_set<int> capitalized;
+    for (int start : starts)
+        dfs(graph, start, capitalized);
+
+    for (vector<int> &word : words)
+        for (int &letter : word)
+            if (capitalized.contains(letter))
+                letter -= letterCount;
+
+    for (int i = 0; i + 1 < words.size(); i++) {
+        if (words[i] > words[i + 1]) {
+            cout << "No";
+            return 0;
+        }
+    }
+
+    cout << "Yes\n" << capitalized.size() << "\n";
+    for (int letter : capitalized)
+        cout << letter + 1 << " ";
+}
---
+++ Codeforces/948_c.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <string>
+using namespace std;
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+  
+    int dayCount;
+    cin >> dayCount;
+
+    vector<int> volumes(dayCount);
+    for (int &volume : volumes)
+        cin >> volume;
+
+    multiset<long long> volumeQueue;
+    long long totalDelta = 0;
+
+    for (int i = 0; i < dayCount; i++) {
+        volumeQueue.insert(volumes[i] + totalDelta);
+
+        int delta;
+        cin >> delta;
+
+        long long res = 0;
+        while (!volumeQueue.empty() && *volumeQueue.begin() - totalDelta <= delta) {
+            res += *volumeQueue.begin() - totalDelta;
+            volumeQueue.erase(volumeQueue.begin());
+        }
+        res += volumeQueue.size() * delta;
+
+        cout << res << " ";
+
+        totalDelta += delta;
+    }
+}
